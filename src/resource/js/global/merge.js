9c7c900aca9b973f4096de1bf079ceec7ec69153
yuyu <yuyu1911@gmail.com>
2012-09-05 11:11:30 +0800

    导入Energon库

--- src/resource/js/global/merge.js
+++ src/resource/js/global/merge.js
@@ -2,8 +2,6 @@
 	importJavascript = function(url){
 		document.write('<script src="'+url+'"></scr'+'ipt>');
 	};
-	importJavascript('/js/base/jquery-1.7.2.min.js');
 	importJavascript('/js/base/bootstrap.min.js');
 	importJavascript('/js/base/highcharts.js');
-	importJavascript('/js/base/jquery.jstree.js');
 })();
